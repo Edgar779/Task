016e4a3a726231d14519b2f9c196e74cfa1f3209
Edgar779 <41956733+Edgar779@users.noreply.github.com>
2018-07-31 21:04:50 +0400

    v1

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 MongoClient.connect(db.url,(err,database) =>{
 
     if (err) return console.log(err)
+    const PORT = process.env.PORT || 3000
     require('./app/routes')(app, database);
-    app.listen(3000,() => {
+    app.listen(PORT,() => {
         console.log("We are live on");
     });
 
